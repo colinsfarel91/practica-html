1f670109f47339acf2589de9463e20b55ec1e361
adrian colinas <adrian.colinas.fg@gmail.com>
2019-08-16 09:24:44 +0200

    update

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+new TypeIt('#replaceStrings', {
+    strings: ["experiencias digitales", "arquitectura web"],
+    speed: 100,
+    breakLines: false,
+    waitUntilVisible: true,
+    loop: true,
+  }).go();
